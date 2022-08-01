3b3051c8d863c5245f32b699c35bcdf5553ed390
Le-manh-duy <93904455+Le-manh-duy@users.noreply.github.com>
2022-08-01 15:24:11 +0700

    bài thi

---
+++ exam/config.php
@@ -0,0 +1,9 @@
+<?php
+$dataHost = 'localhost';
+$dataUserName = 'root';
+$dataPassWord = '';
+$dataName = 'student_db01';
+
+$mysqli = mysqli_connect($dataHost,$dataUserName,$dataPassWord,$dataName);
+
+?>
